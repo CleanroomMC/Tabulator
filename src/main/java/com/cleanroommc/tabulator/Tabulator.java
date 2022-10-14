17d4998442624ac816351ffe507641e99814db1c
brachy84 <brachy84@gmail.com>
2022-10-14 10:52:42 +0200

    1.0.3

--- src/main/java/com/cleanroommc/tabulator/Tabulator.java
+++ src/main/java/com/cleanroommc/tabulator/Tabulator.java
@@ -19,7 +19,7 @@ public class Tabulator {
 
     public static final String ID = "tabulator";
     public static final String NAME = "Tabulator";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     public static final Logger LOGGER = LogManager.getLogger(ID);
 
